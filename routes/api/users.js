5cb95c3a1e57b9ed6ab47d556ea4cc68cd2c3414
monikamaheshwari <monikamaheshwari1996@gmail.com>
2018-12-11 12:01:33 +0530

    email and password login

--- routes/api/users.js
+++ routes/api/users.js
@@ -23,7 +23,7 @@ router.post('/register', (req,res) => {
       if(user){
         return res.status(400).json({email:'Email already exists.'})
       } else {
-        
+
         const avatar = gravatar.url(req.body.email, {
           s: '200', //Size
           r: 'pg', //Rating
@@ -50,6 +50,31 @@ router.post('/register', (req,res) => {
       }
     })
     .catch(err => console.log(err));
+});
+
+// @route GET api/users/login
+// @desc Login user / Returning JWT token
+// @access Public
+router.post('/login',(req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email:email})
+  .then(user => {
+    //Check user
+    if(!user){
+      return res.status(404).json({email:'User not found'});
+    }
+    //Check for password
+    bcrypt.compare(password, user.password)
+    .then(isMatch => {
+      if(isMatch){
+        res.json({msg:'Success'})
+      } else {
+        return res.status(400).json({password:'Password incorrect'});
+      }
+    })
+  })
 });
 
 module.exports = router;
