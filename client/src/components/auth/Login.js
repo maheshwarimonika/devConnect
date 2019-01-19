73680a36c9d1edab27c52dfee76d802979ec8f6f
monikamaheshwari <monikamaheshwari1996@gmail.com>
2019-01-19 23:47:39 +0530

    TextFieldGroup

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
+//import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Login extends Component {
 
@@ -60,24 +61,24 @@ class Login extends Component {
               <h1 className="display-4 text-center">Log In</h1>
               <p className="lead text-center">Sign in to your DevConnector account</p>
               <form onSubmit={this.onSubmit}>
-                <div className="form-group">
-                  <input type="email" className = {classnames('form-control form-control-lg',{
-                      'is-invalid':errors.email
-                    })} value={this.state.email} onChange={this.onChange} placeholder="Email Address" name="email" />
-                  {errors.email && (
-                    <div className="invalid-feedback">{errors.email}</div>
-                    )
-                  }
-                </div>
-                <div className="form-group">
-                  <input type="password" className = {classnames('form-control form-control-lg',{
-                      'is-invalid':errors.password
-                    })} value={this.state.password} onChange={this.onChange} placeholder="Password" name="password" />
-                  {errors.password && (
-                    <div className="invalid-feedback">{errors.password}</div>
-                    )
-                  }
-                </div>
+                <TextFieldGroup
+                  placeholder="Email Address"
+                  name="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  error={errors.email}
+                />
+
+                <TextFieldGroup
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
+
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,7 +18,7 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <a href="" onClick={this.onLogoutClick.bind(this)} className="nav-link"><img src={user.avatar} alt={user.name}
+          <a onClick={this.onLogoutClick.bind(this)} className="nav-link"><img src={user.avatar} alt={user.name}
             className="rounded-circle"
             style={{ width: '25px', marginRight: '5px'}} title="You must have a Gravatar connected to your email to display an image"/>
              Logout
